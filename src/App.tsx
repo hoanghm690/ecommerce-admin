29cfc2093728790b2bdf428aebc56b8a11525720
Hoang Huynh <hoanghuynh0609@gmail.com>
2025-03-19 18:07:12 +0700

    Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,42 @@ import PublicGuard from './components/guard/public-guard'
 import PrivateGuard from './components/guard/private-guard'
 import { lazy, Suspense } from 'react'
 import { Loading } from './components/loading'
+import { ThemeProvider } from './components/theme-provider'
 
 const Dashboard = lazy(() => import('./pages/dashboard'))
 const Login = lazy(() => import('./pages/auth/login'))
 
 function App() {
   return (
-    <Suspense fallback={<Loading />}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navigate to='/dashboard' />} />
-          <Route
-            path='/dashboard'
-            element={
-              <PrivateGuard>
-                <DashboardLayout />
-              </PrivateGuard>
-            }
-          >
-            <Route index element={<Dashboard />} />
-          </Route>
-          <Route
-            element={
-              <PublicGuard>
-                <AuthLayout />
-              </PublicGuard>
-            }
-          >
-            <Route path='/login' element={<Login />} />
-          </Route>
-          <Route path='*' element={<h1>404 - Not Found</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </Suspense>
+    <ThemeProvider>
+      <Suspense fallback={<Loading />}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Navigate to='/dashboard' replace />} />
+            <Route
+              path='/dashboard'
+              element={
+                <PrivateGuard>
+                  <DashboardLayout />
+                </PrivateGuard>
+              }
+            >
+              <Route index element={<Dashboard />} />
+            </Route>
+            <Route
+              element={
+                <PublicGuard>
+                  <AuthLayout />
+                </PublicGuard>
+              }
+            >
+              <Route path='/login' element={<Login />} />
+            </Route>
+            <Route path='*' element={<h1>404 - Not Found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ThemeProvider>
   )
 }
 
--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -1,23 +1,27 @@
 import { AppSidebar } from '@/components/app-sidebar'
+import { ModeToggle } from '@/components/mode-toggle'
 import { SiteHeader } from '@/components/site-header'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { Outlet } from 'react-router'
 
 function DashboardLayout() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <SiteHeader />
-        <div className='flex flex-1 flex-col'>
-          <div className='@container/main flex flex-1 flex-col gap-2'>
-            <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
-              <Outlet />
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <SiteHeader />
+          <div className='flex flex-1 flex-col'>
+            <div className='@container/main flex flex-1 flex-col gap-2'>
+              <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
+                <Outlet />
+              </div>
             </div>
           </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+        </SidebarInset>
+      </SidebarProvider>
+      <ModeToggle />
+    </>
   )
 }
 
