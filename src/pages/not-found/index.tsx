f15d0df9bdbfe2c18c9054e3e45bae7991544a42
Hoang Huynh <hoanghuynh0609@gmail.com>
2025-03-24 13:40:58 +0700

    Not found UI

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,5 +1,28 @@
+import { Button } from '@/components/ui/button'
+import appRoutes from '@/config/routes'
+import { Link } from 'react-router'
+
 function NotFound() {
-  return <h1>NotFound page</h1>
+  return (
+    <section className='w-full'>
+      <div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6'>
+        <div className='mx-auto max-w-screen-sm text-center'>
+          <h1 className='mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500'>
+            404
+          </h1>
+          <p className='mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>
+            Something's missing.
+          </p>
+          <p className='mb-8 text-lg font-light text-gray-500 dark:text-gray-400'>
+            Sorry, we can't find that page. You'll find lots to explore on the dashboard page.{' '}
+          </p>
+          <Button asChild>
+            <Link to={appRoutes.dashboard}>Back to Dashboard</Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default NotFound
